c095b1abd497a2493e0c883d585fdcc0b15af085
Jitendra2407 <jitendrakumarthakur552@gmail.com>
2025-09-29 01:24:14 +0530

    final code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,39 +1,3 @@
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-// import Navbar from "../components/Navbar";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
-// export const metadata = {
-//   title: "Interview App",
-//   description: "Base project with Interviewee & Interviewer routes",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
-//       >
-//         {/* Navbar at the top */}
-//         <Navbar />
-
-//         {/* Main content */}
-//         <main className="max-w-5xl mx-auto p-6">{children}</main>
-//       </body>
-//     </html>
-//   );
-// }
-
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
